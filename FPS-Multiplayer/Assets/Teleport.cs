65499da057c47f537f4e8cc70910da5c997a3f64
Abel 'Riiuk' Santiago <santiagofuentesabel@gmail.com>
2017-11-25 21:55:25 +0100

    Añadidos prefabs personajes
    Añadido los prefabs base de los 8 personajes. Empezado a modificar el HUD para poder seleccionarlos todos.

---
+++ FPS-Multiplayer/Assets/Teleport.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+
+    public Transform teleportPoint;     // Variable tipo Transform que almacena la posición de salida del Teleport
+    
+    /// <summary>
+    /// Método de tipo OnTrigger que almacena la información del objeto con el que colisiona
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerEnter(Collider other)
+    {
+        // Si el objeto colisionado tiene el tag de "Player"
+        if (other.CompareTag("Player"))
+        {
+            // Cambia su posición a la establecida con el punto de teleport
+            other.transform.position = teleportPoint.position;
+        }
+    }
+}
