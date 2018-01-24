55fa53991de64d72644fa9eefb31befb7530cb3a
Abel 'Riiuk' Santiago <santiagofuentesabel@gmail.com>
2018-01-24 13:13:03 +0100

    GARNesha
    Habilidad lista en multiplayer

---
+++ FPS-Multiplayer/Assets/Scripts/Missile.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class Missile : MonoBehaviour
+{
+    [Header("Explosión")]
+    [Tooltip("Radio de la explosión")]
+    public float radio = 5f;
+    [Tooltip("Velocidad del misil")]
+    public float speed = 10f;
+    [Tooltip("Fuerza de la explosión")]
+    public float fuerzaExplosion = 700f;
+    [Tooltip("Daño del misil")]
+    public float daño = 50f;
+
+    [Header("Prefabs")]
+    [Tooltip("Prefab del Sistema de Particulas para la explosion")]
+    public GameObject prefabExplosionEffect;
+    // Variable que usamos para avisar de que la granada ya ha explotado
+    bool haExplotado = false;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+    void Explotar()
+    {
+        haExplotado = true;
+
+        Instantiate(prefabExplosionEffect, transform.position, transform.rotation);
+
+        Collider[] collidersDestruir = Physics.OverlapSphere(transform.position, radio);
+
+        foreach (Collider objetoEnRango in collidersDestruir)
+        {
+
+            Destructible destruible = objetoEnRango.GetComponent<Destructible>();
+
+            if (destruible != null)
+            {
+
+                destruible.Destruir();
+            }
+        }
+
+        Collider[] collidersMover = Physics.OverlapSphere(transform.position, radio);
+
+        foreach (Collider objetoEnRango in collidersMover)
+        {
+
+            NetPlayerHealth enemy = objetoEnRango.GetComponent<NetPlayerHealth>();
+
+            if (enemy != null)
+            {
+
+                // El método TakeDamage devuelve true, si somos nosotros los que lo hemos matado
+                if (enemy.TakeDamage(daño))
+                {
+                    // Incrementamos el número de muertes
+                    GetComponentInParent<Score>().kills++;
+                }
+            }
+        }
+
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Explotar();
+    }
+
+    /// <summary>
+    /// Función para dibujar Gizmos al tener seleccionado el objeto
+    /// </summary>
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radio);
+    }
+}
---
+++ FPS-Multiplayer/Assets/Scripts/NetSkills/NetSkillLADYMet.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+// Heredará de NetworkBehaviour
+public class NetSkillLADYMet : NetworkBehaviour
+{
+
+   
+    // Tiempo de enfriamiento de la habilidad
+    public float coolDown = 30f;
+    public float spawnDelay = 0.5f;
+    public int cantidadMisiles = 5;
+    // Imagen que indicará el tiempo restante de CoolDown
+    public Image coolDownImage;
+    // Sistema de partículas que será mostrado al usar la skill
+    public ParticleSystem skillEffect;
+
+    public GameObject missile;
+    public Transform misileSpawnPoint;
+    // Contador interno de tiempo de enfriamiento
+    private float coolDownTimer;
+
+
+    private NetPlayerHealth netPlayerHealth;
+
+    void Start()
+    {
+        netPlayerHealth = GetComponent<NetPlayerHealth>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Si no es el jugador local, salimos dle update sin hacer nada
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        // Si el jugador pulsa el botón Hability1 y si el temporizador ha terminado
+        if (Input.GetButtonDown("Hability1") && coolDownTimer <= 0 && netPlayerHealth.stunned <= 0)
+        {
+            CmdUseSkill();
+        }
+        // Si el contador es mayor que 0 realizamos las operaciones de dibujado del CooldownImage
+        if (coolDownTimer > 0)
+        {
+            coolDownTimer -= Time.deltaTime;
+            // Mostramos visualmente el tiempo restante para que la habilidad esté disponible
+            coolDownImage.fillAmount = coolDownTimer / coolDown;
+        } else
+        {
+            coolDownImage.fillAmount = 0;
+        }
+    }
+
+    IEnumerator MissileLauncher()
+    {
+        for (int i = 0; i < cantidadMisiles; i++)
+        {
+            Instantiate(missile, misileSpawnPoint.position, misileSpawnPoint.rotation);
+            yield return new WaitForSeconds(spawnDelay);
+        }
+        
+    }
+
+    [Command]
+    /// <summary>
+    /// Comando ejecutado en el servidor que será el encargado de verificar que los jugadores han sido stuneados
+    /// </summary>
+    public void CmdUseSkill()
+    {
+        //MissileLauncher();
+
+        StartCoroutine("MissileLauncher");
+
+        // Ponemos el contador de tiempo de coolDown al valor definido
+        coolDownTimer = coolDown;
+        // Activamos los efectos de la habilidad
+        SkillEffect();
+        // Solicitamos que se realicen las acciones necesarias en las instancias
+        RpcUseSkill();
+
+    }
+
+    [ClientRpc]
+    /// <summary>
+    /// Activamos el efecto de skill y el reseteo del cooldown al resto de instancias
+    /// </summary>
+    public void RpcUseSkill()
+    {
+        // Mostramos efectos de la habildiad
+        SkillEffect();
+        // Reseteamos el temporizador
+        coolDownTimer = coolDown;
+    }
+
+    /// <summary>
+    /// Activa los efectos asociados a la habilidad
+    /// </summary>
+    public void SkillEffect()
+    {
+        // Activamos el sistema de partículas
+        skillEffect.Play();
+    }
+}
+
